a360031b8e937201c22b0c84e4eaf73d42b162e7
Arya Solhi <aryasolhi@gmail.com>
2015-09-19 10:02:22 -0400

    Made a ton of changes, this is stable though

--- public_html/app/corejs/app.js
+++ public_html/app/corejs/app.js
@@ -2,19 +2,63 @@
 'use strict';
 
     var de = angular.module("de", [
+        'firebase',
         'ngRoute'
     ]);
+    
+    function ri(page){
+        return {
+            templateUrl: "app/"+page+"/temp.html",
+            controller: page+' as '+page
+        };
+    }
 
     de.config(['$routeProvider','$locationProvider',
       function($routeProvider,$locationProvider) {
         $locationProvider.html5Mode(true).hashPrefix('!');
         $routeProvider.
-          when('/', {
-            templateUrl: 'app/landingpage/landingpage.html',
-            controller: 'LandingPageController as lpCtrl'
-          }).
+          when('/', ri("lp")).
           otherwise({
             redirectTo: '/404'
           });
       }]);
+    
+    de.factory("Auth", ["$firebaseAuth",
+      function($firebaseAuth) {
+        var authRef = new Firebase("https://digitalemerald.firebaseio.com");
+        return $firebaseAuth(authRef);
+      }
+    ]);
+    
+    
+    de.controller('MainController', ["$scope", "$firebaseObject", "Auth", function($scope, $firebaseObject, Auth){
+        
+        var main = this;
+        main.client = {};
+        var clientRef = "";
+
+        main.auth = Auth;
+        main.auth.$onAuth(function(authData) {
+            clientRef = new Firebase("https://digitalemerald.firebaseio.com/" + authData.auth.uid);
+            main.authData = authData;
+            main.client = $firebaseObject(clientRef);
+            main.client.name = authData.google.displayName;
+            main.client.$save();
+        });
+        
+        main.login = function(){
+            main.auth.$authWithOAuthPopup("google").then(function(authData) {
+                console.log("Logged in as:", authData.uid);
+            }).catch(function(error) {
+                console.error("Authentication failed:", error);
+            });
+        }
+        
+    }])
+    
+    
+    
+    de.controller('lp', ["$scope", function($scope){
+        $scope.yolo = 'hollla';
+    }])
 })()
